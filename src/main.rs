7e72e4f13326beef13b1537931ec500d47b75142
Pedro Godoy <pedrohenrique.godoy@outlook.com>
2022-10-26 19:49:16 -0300

    refactor: improve env acquirement visibility

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ mod infra;
 async fn main() {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse::<u16>()
+        .unwrap();
 
     let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(10)
@@ -22,12 +27,6 @@ async fn main() {
     let broker = Arc::new(infra::broker::AMQPBroker::new().await);
     let app_service = Arc::new(ApplicationService::new(item_repo, broker));
 
-    let options = http::HttpOptions {
-        host: env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string()),
-        port: env::var("PORT")
-            .unwrap_or_else(|_| "8080".to_string())
-            .parse::<u16>()
-            .unwrap(),
-    };
+    let options = http::HttpOptions { host, port };
     http::setup(app_service, options).await.unwrap();
 }
